03006bf660dc89b9fd8e4fec6c669e7080909b1b
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-01-31 10:59:25 -0300

    Proyecto Inicial
    Base Clase 05

---
+++ CLASE 06 - INPUT/Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Vector3 direction = new Vector3(0, 0, 1);
+    [SerializeField] float speed = 2f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        transform.Translate(speed * Time.deltaTime * direction);
+    }
+}
